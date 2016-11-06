c3234eee80480f777bc0594ac7e07230b85b0278
Derrick Mar <derrickmar1215@berkeley.edu>
2016-11-05 21:16:18 -0700

    Changes based on pairing with Sam

--- jupyterhub-k8s-hub/jupyterhub_config.py
+++ jupyterhub-k8s-hub/jupyterhub_config.py
@@ -5,7 +5,7 @@ c = get_config()
 
 import os
 import sys
-sys.path.insert(0, '/srv/oauthenticator')
+# sys.path.insert(0, '/srv/oauthenticator')
 
 # Base configuration
 c.JupyterHub.log_level = "INFO"
@@ -14,19 +14,27 @@ c.JupyterHub.admin_access = True
 c.JupyterHub.confirm_no_ssl = True
 c.JupyterHub.proxy_check_interval = 30
 # Use the nginx based proxy, rather than the nodejs one
-c.JupyterHub.proxy_cmd = '/opt/conda/bin/nchp'
+c.JupyterHub.proxy_cmd = '/usr/local/bin/nchp'
+
+# Proxy and hub is running on the same machine
+c.JupyterHub.ip = '0.0.0.0'
 
 # Configure the authenticator
-c.JupyterHub.authenticator_class = 'docker_oauth.DockerOAuthenticator'
+c.JupyterHub.authenticator_class = 'oauthenticator.GoogleOAuthenticator'
 
-print os.environ['OAUTH_CALLBACK_URL']
-
-c.DockerOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-c.DockerOAuthenticator.create_system_users = True
-c.Authenticator.admin_users = admin = set()
-#c.Authenticator.whitelist = whitelist = set()
+c.GoogleOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 c.GoogleOAuthenticator.hosted_domain = 'berkeley.edu'
 c.GoogleOAuthenticator.login_service = 'UC Berkeley'
+
+# print os.environ['OAUTH_CALLBACK_URL']
+
+# c.DockerOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+# c.DockerOAuthenticator.create_system_users = True
+
+# TODO: Look into what these do
+# c.Authenticator.admin_users = admin = set()
+# c.Authenticator.whitelist = whitelist = set()
+
 
 ### KUBESPAWNER STUFF
 # Configure the spawner
@@ -45,8 +53,7 @@ c.JupyterHub.cleanup_servers = False
 # First pulls can be really slow, so let's give it a big timeout
 c.KubeSpawner.start_timeout = 60 * 5
 
-# Our simplest user image! Optimized to just... start, and be small!
-c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
+c.KubeSpawner.singleuser_image_spec = 'data8/systemuser'
 
 # The spawned containers need to be able to talk to the hub, ok through the proxy!
 c.KubeSpawner.hub_ip_connect = '{host}:{port}'.format(
@@ -80,8 +87,8 @@ c.KubeSpawner.hub_ip_connect = '{host}:{port}'.format(
 # doesn't work. We need to tell the hub to listen on 0.0.0.0 because it's in a
 # container, and we'll expose the port properly when the container is run. Then,
 # we explicitly tell the spawned containers to connect to the proper IP address.
-c.JupyterHub.proxy_api_ip = '0.0.0.0'
-c.JupyterHub.hub_ip = '0.0.0.0'
+# c.JupyterHub.proxy_api_ip = '0.0.0.0'
+# c.JupyterHub.hub_ip = '0.0.0.0'
 
 ###
 # c.DockerSpawner.hub_ip_connect = os.environ['HUB_IP']
@@ -89,11 +96,11 @@ c.JupyterHub.hub_ip = '0.0.0.0'
 
 # Add users to the admin list, the whitelist, and also record their user ids
 
-with open('/srv/oauthenticator/userlist') as f:
-    for line in f:
-        if line.isspace(): continue
-        parts = line.split()
-        #whitelist.add(name)
-        if len(parts) > 1 and parts[1] == 'admin':
-            admin.add(parts[0])
+# with open('/srv/oauthenticator/userlist') as f:
+#     for line in f:
+#         if line.isspace(): continue
+#         parts = line.split()
+#         #whitelist.add(name)
+#         if len(parts) > 1 and parts[1] == 'admin':
+#             admin.add(parts[0])
 
