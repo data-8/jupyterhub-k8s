4566e9dce723836d7f34d855799df848b57cd1ea
Mark Lescroart <mark.lescroart@gmail.com>
2017-05-10 09:48:44 -0700

    Update to add data for final exam; also added comments to installed packages in neuro.bash and 2 more package installs that are dependencies for software we're beta testing for next semester

--- disks/cogneuro.bash
+++ disks/cogneuro.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-# Tweaked an experiment design file
-wget https://www.dropbox.com/s/6viqotryiy8tdox/cogneuro88data.tar.gz
-tar -xvf cogneuro88data.tar.gz
-rm cogneuro88data.tar.gz
+# Added data for the final exam
+wget https://www.dropbox.com/s/dkyn897kscbp6bc/data8_final.tar.gz
+tar -xvf data8_final.tar.gz
+rm data8_final.tar.gz
--- user/connectors/neuro.bash
+++ user/connectors/neuro.bash
@@ -2,14 +2,16 @@
 # conda & pip packages for neuro connector (mark.lescroart)
 
 ${CONDA_DIR}/bin/pip --no-cache-dir install \
-	lxml==3.7.2 \
-	mne==0.13.1 \
-	tqdm==4.11.2 \
-	;
+    lxml==3.7.2 \ # For pycortex
+    mne==0.13.1 \ # For neurods
+    tqdm==4.11.2 \ # For neurods
+    boto3==1.4.4 \ # For cottoncandy
+    pydrive==1.3.1 \ # For cottoncandy
+    ;
 
 ${CONDA_DIR}/bin/conda install --quiet --yes \
-	nibabel==2.1.0 \
-	;
+    nibabel==2.1.0 \
+    ;
 
 # pycortex can't be installed from the repository at the moment
 # ${CONDA_DIR}/bin/pip install git+https://github.com/gallantlab/pycortex@data8
@@ -20,3 +22,4 @@ cd pycortex
 # commit on data8 branch
 git checkout d570713
 ${CONDA_DIR}/bin/python setup.py install
+cd ..
